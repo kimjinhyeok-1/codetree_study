f95f3f3d4ddc850693a10912bddec4e1dc9edd1e
Jinhyeok Kim <jinxxang123@gmail.com>
2026-07-31 08:19:29 +0900

    [Passed] 네 방향 탈출 가능 여부 판별하기 (57ms, 17MB)

--- trail4/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ trail4/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -3,25 +3,21 @@ from collections import deque
 input = sys.stdin.readline
 
 n,m = map(int, input().split())
-map = [list(map(int,input().split())) for _ in range(n)]
+grid = [list(map(int,input().split())) for _ in range(n)]
 visited = [[False] * m for _ in range(n)]
 visited[0][0] = True
-isTrue = False
+
 q = deque([(0,0)])
 
 d = [(1,0),(-1,0),(0,1),(0,-1)]
 while q: 
     r,c = q.popleft()
-    if r == n-1 and c == m-1:
-        isTrue = True
+
     for dr, dc in d:
         nr, nc = r+dr, c+dc
         if 0<= nr < n and 0<= nc < m:
-            if map[nr][nc] == 1 and visited[nr][nc] == False:
+            if grid[nr][nc] == 1 and visited[nr][nc] == False:
                 visited[nr][nc] = True
                 q.append((nr,nc))
 
-if isTrue:
-    print(1)
-else:
-    print(0) 
+print(1 if visited[n-1][m-1] else 0)
